ba393d63fcabb1515a868d5500474bb2e1f47a88
Trent Mick <trentm@gmail.com>
2008-02-07 19:02:52 +0000

    tweak. No functional change.

--- sandbox/wiki.py
+++ sandbox/wiki.py
@@ -14,10 +14,8 @@ This is AnotherPage and YetAnotherPage.
 
 link_patterns = [
     # Match a wiki page link LikeThis.
-    (re.compile(r"(\b[A-Z][a-z]+[A-Z]\w+\b)"),
-     r"/\1")
+    (re.compile(r"(\b[A-Z][a-z]+[A-Z]\w+\b)"), r"/\1")
 ]
 processor = markdown2.Markdown(extras=["link-patterns"],
                                link_patterns=link_patterns)
-
 print processor.convert(wiki_page)
