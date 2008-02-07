77fb840af383e5a55ab9442ac9c9d1cbfe7c94ed
Trent Mick <trentm@gmail.com>
2008-02-07 18:52:51 +0000

    add a sandbox, play with a WikiWord link pattern

---
+++ sandbox/wiki.py
@@ -0,0 +1,23 @@
+
+import sys
+import re
+from os.path import *
+
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import markdown2
+
+wiki_page = """
+# This is my WikiPage!
+
+This is AnotherPage and YetAnotherPage.
+"""
+
+link_patterns = [
+    # Match a wiki page link LikeThis.
+    (re.compile(r"(\b[A-Z][a-z]+[A-Z]\w+\b)"),
+     r"/\1")
+]
+processor = markdown2.Markdown(extras=["link-patterns"],
+                               link_patterns=link_patterns)
+
+print processor.convert(wiki_page)
