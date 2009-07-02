8e25121bc00ecf1d90e1fb9a277bc391db227676
Trent Mick <trentm@gmail.com>
2009-07-02 06:09:18 +0000

    'mk announce_release' helper task

--- Makefile.py
+++ Makefile.py
@@ -11,6 +11,7 @@ import os
 from os.path import join, dirname, normpath, abspath, exists, basename
 import re
 import webbrowser
+from pprint import pprint
 
 from mklib.common import MkError
 from mklib import Task
@@ -149,9 +150,327 @@ class pygments(Task):
                         % basename(pygments_dir),
                        dirname(pygments_dir), self.log.info)
 
+class announce_release(Task):
+    """Send a release announcement. Don't send this multiple times!."""
+    headers = {
+        "To": ["python-markdown2@googlegroups.com",
+           "python-announce@python.org"],
+        "From": ["Trent Mick <trentm@gmail.com>"],
+        "Subject": "ANN: python-markdown2 %(version)s -- A fast and complete Python implementation of Markdown",
+        "Reply-To": "python-markdown2@googlegroups.com",
+    }
+    if False: # for dev/debugging
+        headers["To"] = ["trentm@gmail.com"]
+    
+    body = r"""
+        ### Where?
+
+        - Project Page: <http://code.google.com/p/python-markdown2/>
+        - PyPI: <http://pypi.python.org/pypi/markdown2/>
+
+        %(whatsnew)s
+        ### What is 'markdown2'?
+        
+        `markdown2.py` is a fast and complete Python implementation of
+        [Markdown](http://daringfireball.net/projects/markdown/) -- a
+        text-to-HTML markup syntax.
+        
+        ### Module usage
+        
+            >>> import markdown2
+            >>> markdown2.markdown("*boo!*")  # or use `html = markdown_path(PATH)`
+            u'<p><em>boo!</em></p>\n'
+        
+            >>> markdowner = Markdown()
+            >>> markdowner.convert("*boo!*")
+            u'<p><em>boo!</em></p>\n'
+            >>> markdowner.convert("**boom!**")
+            u'<p><strong>boom!</strong></p>\n'
+
+        ### Command line usage
+        
+            $ cat hi.markdown
+            # Hello World!
+            $ markdown2 hi.markdown
+            <h1>Hellow World!</h1>
+
+        This implementation of Markdown implements the full "core" syntax plus a
+        number of extras (e.g., code syntax coloring, footnotes) as described on
+        <http://code.google.com/p/python-markdown2/wiki/Extras>.
+
+        Cheers,
+        Trent
+
+        --
+        Trent Mick
+        trentm@gmail.com
+        http://trentm.com/blog/
+    """
+    
+    def _parse_changes_txt(self):
+        changes_txt = open(join(self.dir, "CHANGES.txt")).read()
+        sections = re.split(r'\n(?=##)', changes_txt)
+        for section in sections[1:]:
+            first, tail = section.split('\n', 1)
+            if "not yet released" in first:
+                continue
+            break
+
+        whatsnew_text = "### What's new\n" + tail
+        whatsnew_text = whatsnew_text.rstrip('\n') + '\n\n'
+        version = first.strip().split()[-1]
+        if version.startswith("v"):
+            version = version[1:]
+
+        return version, whatsnew_text
+    
+    def make(self):
+        import getpass
+        if getpass.getuser() != "trentm":
+            raise RuntimeError("You're not `trentm`. That's not "
+                "expected here.")
+
+        version, whatsnew = self._parse_changes_txt()
+        data = {
+            "whatsnew": whatsnew,
+            "version": version,
+        }
+
+        headers = {}
+        for name, v in self.headers.items():
+            if isinstance(v, basestring):
+                value = v % data
+            else:
+                value = v
+            headers[name] = value
+        body = _dedent(self.body, skip_first_line=True) % data
+        
+        # Ensure all the footer lines end with two spaces: markdown syntax
+        # for <br/>.
+        lines = body.splitlines(False)
+        idx = lines.index("Cheers,") - 1
+        for i in range(idx, len(lines)):
+            lines[i] += '  '
+        body = '\n'.join(lines)
+
+        print "=" * 70, "body"
+        print body
+        print "=" * 70
+        answer = _query_yes_no(
+            "Send release announcement email for v%s to %s?" % (
+                version, ", ".join(self.headers["To"])),
+            default="no")
+        if answer != "yes":
+            return
+
+        sys.path.insert(0, join(self.dir, "lib"))
+        import markdown2
+        body_html = markdown2.markdown(body)
+        
+        email_it_via_gmail(headers, text=body, html=body_html)
+        self.log.info("announcement sent")
+
 
 
 #---- internal support stuff
+
+def email_it_via_gmail(headers, text=None, html=None, password=None):
+    """Send an email -- with text and HTML parts.
+    
+    @param headers {dict} A mapping with, at least: "To", "Subject" and
+        "From", header values. "To", "Cc" and "Bcc" values must be *lists*,
+        if given.
+    @param text {str} The text email content.
+    @param html {str} The HTML email content.
+    @param password {str} Is the 'From' gmail user's password. If not given
+        it will be prompted for via `getpass.getpass()`.
+    
+    Derived from http://code.activestate.com/recipes/473810/ and
+    http://stackoverflow.com/questions/778202/smtplib-and-gmail-python-script-problems
+
+    TODO: Make a recipe from this.
+    """
+    from email.MIMEMultipart import MIMEMultipart
+    from email.MIMEText import MIMEText
+    import smtplib
+    import getpass
+    
+    if text is None and html is None:
+        raise ValueError("neither `text` nor `html` content was given for "
+            "sending the email")
+    if not ("To" in headers and "From" in headers and "Subject" in headers):
+        raise ValueError("`headers` dict must include at least all of "
+            "'To', 'From' and 'Subject' keys")
+
+    # Create the root message and fill in the from, to, and subject headers
+    msg_root = MIMEMultipart('related')
+    for name, value in headers.items():
+        msg_root[name] = isinstance(value, list) and ', '.join(value) or value
+    msg_root.preamble = 'This is a multi-part message in MIME format.'
+
+    # Encapsulate the plain and HTML versions of the message body in an
+    # 'alternative' part, so message agents can decide which they want
+    # to display.
+    msg_alternative = MIMEMultipart('alternative')
+    msg_root.attach(msg_alternative)
+
+    # Attach HTML and text alternatives.
+    if text:
+        msg_text = MIMEText(text.encode('utf-8'))
+        msg_alternative.attach(msg_text)
+    if html:
+        msg_text = MIMEText(html.encode('utf-8'), 'html')
+        msg_alternative.attach(msg_text)
+
+    to_addrs = headers["To"] \
+        + headers.get("Cc", []) \
+        + headers.get("Bcc", [])
+    from_addr = msg_root["From"]
+    
+    # Get username and password.
+    from_addr_pats = [
+        re.compile(".*\((.+@.+)\)"),  # Joe (joe@example.com)
+        re.compile(".*<(.+@.+)>"),  # Joe <joe@example.com>
+    ]
+    for pat in from_addr_pats:
+        m = pat.match(from_addr)
+        if m:
+            username = m.group(1)
+            break
+    else:
+        username = from_addr
+    if not password:
+        password = getpass.getpass("%s's password: " % username)
+    
+    smtp = smtplib.SMTP('smtp.gmail.com', 587) # port 465 or 587
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo()
+    smtp.login(username, password)
+    smtp.sendmail(from_addr, to_addrs, msg_root.as_string())
+    smtp.close()
+
+
+# Recipe: dedent (0.1.2)
+def _dedentlines(lines, tabsize=8, skip_first_line=False):
+    """_dedentlines(lines, tabsize=8, skip_first_line=False) -> dedented lines
+    
+        "lines" is a list of lines to dedent.
+        "tabsize" is the tab width to use for indent width calculations.
+        "skip_first_line" is a boolean indicating if the first line should
+            be skipped for calculating the indent width and for dedenting.
+            This is sometimes useful for docstrings and similar.
+    
+    Same as dedent() except operates on a sequence of lines. Note: the
+    lines list is modified **in-place**.
+    """
+    DEBUG = False
+    if DEBUG: 
+        print "dedent: dedent(..., tabsize=%d, skip_first_line=%r)"\
+              % (tabsize, skip_first_line)
+    indents = []
+    margin = None
+    for i, line in enumerate(lines):
+        if i == 0 and skip_first_line: continue
+        indent = 0
+        for ch in line:
+            if ch == ' ':
+                indent += 1
+            elif ch == '\t':
+                indent += tabsize - (indent % tabsize)
+            elif ch in '\r\n':
+                continue # skip all-whitespace lines
+            else:
+                break
+        else:
+            continue # skip all-whitespace lines
+        if DEBUG: print "dedent: indent=%d: %r" % (indent, line)
+        if margin is None:
+            margin = indent
+        else:
+            margin = min(margin, indent)
+    if DEBUG: print "dedent: margin=%r" % margin
+
+    if margin is not None and margin > 0:
+        for i, line in enumerate(lines):
+            if i == 0 and skip_first_line: continue
+            removed = 0
+            for j, ch in enumerate(line):
+                if ch == ' ':
+                    removed += 1
+                elif ch == '\t':
+                    removed += tabsize - (removed % tabsize)
+                elif ch in '\r\n':
+                    if DEBUG: print "dedent: %r: EOL -> strip up to EOL" % line
+                    lines[i] = lines[i][j:]
+                    break
+                else:
+                    raise ValueError("unexpected non-whitespace char %r in "
+                                     "line %r while removing %d-space margin"
+                                     % (ch, line, margin))
+                if DEBUG:
+                    print "dedent: %r: %r -> removed %d/%d"\
+                          % (line, ch, removed, margin)
+                if removed == margin:
+                    lines[i] = lines[i][j+1:]
+                    break
+                elif removed > margin:
+                    lines[i] = ' '*(removed-margin) + lines[i][j+1:]
+                    break
+            else:
+                if removed:
+                    lines[i] = lines[i][removed:]
+    return lines
+
+def _dedent(text, tabsize=8, skip_first_line=False):
+    """_dedent(text, tabsize=8, skip_first_line=False) -> dedented text
+
+        "text" is the text to dedent.
+        "tabsize" is the tab width to use for indent width calculations.
+        "skip_first_line" is a boolean indicating if the first line should
+            be skipped for calculating the indent width and for dedenting.
+            This is sometimes useful for docstrings and similar.
+    
+    textwrap.dedent(s), but don't expand tabs to spaces
+    """
+    lines = text.splitlines(1)
+    _dedentlines(lines, tabsize=tabsize, skip_first_line=skip_first_line)
+    return ''.join(lines)
+
+
+# Recipe: query_yes_no (1.0)
+def _query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+    
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":"yes",   "y":"yes",  "ye":"yes",
+             "no":"no",     "n":"no"}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while 1:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return default
+        elif choice in valid.keys():
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
 
 # Recipe: paths_from_path_patterns (0.3.7)
 def _should_include_path(path, includes, excludes):
