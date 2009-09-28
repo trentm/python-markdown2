b0001568bbf1e777e72a670fb35f018db5b20a07
Trent Mick <trentm@gmail.com>
2009-09-28 21:42:57 +0000

    Be better about closing file handles (part of a patch from Massimo Di Pierro).

--- lib/markdown2.py
+++ lib/markdown2.py
@@ -109,7 +109,9 @@ def markdown_path(path, encoding="utf-8",
                   html4tags=False, tab_width=DEFAULT_TAB_WIDTH,
                   safe_mode=None, extras=None, link_patterns=None,
                   use_file_vars=False):
-    text = codecs.open(path, 'r', encoding).read()
+    fp = codecs.open(path, 'r', encoding)
+    text = fp.read()
+    fp.close()
     return Markdown(html4tags=html4tags, tab_width=tab_width,
                     safe_mode=safe_mode, extras=extras,
                     link_patterns=link_patterns,
