8b367378811f6073e7a2b26e014912e7097b3fca
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-10-15 20:58:00 +0300

    Add support for Python 3.8-3.9

--- setup.py
+++ setup.py
@@ -26,6 +26,8 @@ Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Operating System :: OS Independent
 Topic :: Software Development :: Libraries :: Python Modules
 Topic :: Software Development :: Documentation
