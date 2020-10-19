91d485466ec230d66b898b2774c0acec7916c09f
Nicholas Serra <nickserra@gmail.com>
2020-10-19 16:21:22 -0400

    Update setup.py restrict to Python 3.5 and less than 4

--- setup.py
+++ setup.py
@@ -17,12 +17,7 @@ Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
 Programming Language :: Python
-Programming Language :: Python :: 2
-Programming Language :: Python :: 2.6
-Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.3
-Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
@@ -51,7 +46,7 @@ setup(
     package_dir={"": "lib"},
     scripts=[script],
     description="A fast and complete Python implementation of Markdown",
-    python_requires=">=2.6, !=3.0.*, !=3.1.*, !=3.2.*",
+    python_requires=">=3.5, <4",
     classifiers=classifiers.strip().split("\n"),
     long_description="""markdown2: A fast and complete Python implementation of Markdown.
 
