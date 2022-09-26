8deb46b712c980ccb95f5ad8538eecd130d2349a
Crozzers <captaincrozzers@gmail.com>
2022-09-26 19:48:40 +0100

    Limit wavedrom installs to Python 3.7 and above

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ Topic :: Text Processing :: Markup :: HTML
 
 extras_require = {
     "code_syntax_highlighting": ["pygments>=2.7.3"],
-    "wavedrom": ["wavedrom"]
+    "wavedrom": ["wavedrom; python_version>='3.7'"]
 }
 # nested listcomp to combine all optional extras into convenient "all" option
 extras_require["all"] = [i for v in tuple(extras_require.values()) for i in v]
