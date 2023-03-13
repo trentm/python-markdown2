b5cbd8e0da872afa24e703cfbb0e60d87c415440
Crozzers <captaincrozzers@gmail.com>
2023-03-13 09:04:02 +0000

    Restore original usage message and re-add version arg

--- lib/markdown2.py
+++ lib/markdown2.py
@@ -2241,7 +2241,7 @@ class Markdown(object):
     def _do_underline(self, text):
         text = self._underline_re.sub(r"<u>\1</u>", text)
         return text
-    
+
     _tg_spoiler_re = re.compile(r"\|\|\s?(.+?)\s?\|\|", re.S)
     def _do_tg_spoiler(self, text):
         text = self._tg_spoiler_re.sub(r"<tg-spoiler>\1</tg-spoiler>", text)
@@ -2974,9 +2974,11 @@ def main(argv=None):
         logging.basicConfig()
 
     parser = argparse.ArgumentParser(
-        prog="markdown2", description=cmdln_desc,
+        prog="markdown2", description=cmdln_desc, usage='%(prog)s [PATHS...]',
         formatter_class=_NoReflowFormatter
     )
+    parser.add_argument('--version', action='version',
+                        version='%(prog)s {version}'.format(version=__version__))
     parser.add_argument('paths', nargs='*',
                         help=(
                             'optional list of files to convert.'
