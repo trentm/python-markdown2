d8ec9b646f9eb41ccfec0e1a7b5c1ddda3d3d2b7
Navan Chauhan <navanchauhan@gmail.com>
2024-03-26 17:54:08 -0600

    add latex extra

--- lib/markdown2.py
+++ lib/markdown2.py
@@ -2764,6 +2764,51 @@ class FencedCodeBlocks(Extra):
     def run(self, text):
         return self.fenced_code_block_re.sub(self.sub, text)
 
+class Latex(Extra):
+    '''
+    Convert $ and $$ to <math> and </math> tags for inline and block math.
+    '''
+    name = 'latex'
+    order = (), ()
+
+    _single_dollar_re = re.compile(r'(?<!\$)\$(?!\$)(.*?)\$')
+    _double_dollar_re = re.compile(r'\$\$(.*?)\$\$', re.DOTALL)
+    # Used to disable conversion inside code blocks
+    _pre_code_block_re = re.compile(r"<pre>(.*?)</pre>", re.DOTALL)
+
+    converter = None
+
+    def _convert_single_match(self, match):
+        return self.converter.convert(match.group(1))
+
+    def _convert_double_match(self, match):
+        return self.converter.convert(match.group(1).replace(r"\n", ''), display="block")
+
+    def code_placeholder(self, match):
+        placeholder = f"<!--CODE_BLOCK_{len(code_blocks)}-->"
+        code_blocks[placeholder] = match.group(0)
+        return placeholder
+
+    def run(self, text):
+        try:
+            import latex2mathml.converter
+            self.converter = latex2mathml.converter
+        except ImportError:
+            raise ImportError('The "latex" extra requires the "latex2mathml" package to be installed.')
+
+        code_blocks = {}
+
+        # Replace code blocks with placeholder tag
+        text = self._pre_code_block_re.sub(self.code_placeholder, text)
+
+        text = self._single_dollar_re.sub(self._convert_single_match, text)
+        text = self._double_dollar_re.sub(self._convert_double_match, text)
+
+        # Convert placeholder tag back to original code
+        for placeholder, code_block in code_blocks.items():
+            text = text.replace(placeholder, code_block)
+
+        return text
 
 class LinkPatterns(Extra):
     '''
@@ -3328,6 +3373,7 @@ Admonitions.register()
 Breaks.register()
 CodeFriendly.register()
 FencedCodeBlocks.register()
+Latex.register()
 LinkPatterns.register()
 MarkdownInHTML.register()
 MiddleWordEm.register()
